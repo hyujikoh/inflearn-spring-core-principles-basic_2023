8f3eba6e4cb90494a72676b6a112371421516b0b
hyujikoh <dvum0045@gmail.com>
2023-08-17 20:30:35 +0900

    init commit

---
+++ src/main/java/ohj/core/springoop/SpringOopApplication.java
@@ -0,0 +1,13 @@
+package ohj.core.springoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ohj/core/springoop/SpringOopApplicationTests.java
@@ -0,0 +1,13 @@
+package ohj.core.springoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
