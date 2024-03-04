1b2b72eaf8a5f5e6edccad82c860cd70d99aa2ac
hyujikoh <dvum0045@gmail.com>
2024-03-04 15:21:28 +0900

    Study - 새로운 할인 정책 개발, 기존 고정값인 할인율을 가격에 따른 할인율을 적용

--- src/main/java/ohj/core/springoop/order/OrderServiceImpl.java
+++ src/main/java/ohj/core/springoop/order/OrderServiceImpl.java
@@ -13,7 +13,7 @@ import ohj.core.springoop.member.respository.MemoryMemberRepository;
  */
 public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
