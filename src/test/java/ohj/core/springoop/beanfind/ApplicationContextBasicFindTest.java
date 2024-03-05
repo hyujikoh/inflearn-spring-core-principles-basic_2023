d6be108d8a8e7ba7d1d08ed558b5d7eb6db7e7c4
hyujikoh <dvum0045@gmail.com>
2024-03-05 09:47:55 +0900

    Study - 빈 조회 방법에 대한 테스트

---
+++ src/test/java/ohj/core/springoop/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,47 @@
+package ohj.core.springoop.beanfind;
+
+import ohj.core.springoop.AppConfig;
+import ohj.core.springoop.member.service.MemberService;
+import ohj.core.springoop.member.service.MemberServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Author : hyujikoh
+ * CreatedAt : 2024-03-05
+ * Desc :
+ */
+public class ApplicationContextBasicFindTest {
+    AnnotationConfigApplicationContext ac = new
+            AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean(MemberService.class);
+
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        MemberServiceImpl memberService = ac.getBean("memberService",
+                MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanByNameX() {
+        Assertions.assertThrows(NoSuchBeanDefinitionException.class, () ->
+                ac.getBean("xxxxx", MemberService.class));
+    }
+}
