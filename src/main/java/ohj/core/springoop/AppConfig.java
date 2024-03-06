89e46dbc2b918df48a38d52741331067f9563d31
hyujikoh <dvum0045@gmail.com>
2024-03-06 14:02:07 +0900

    Study - 테스트를 위한 출력

--- src/main/java/ohj/core/springoop/AppConfig.java
+++ src/main/java/ohj/core/springoop/AppConfig.java
@@ -21,11 +21,15 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
     @Bean
     public MemberService memberService() {
+
+        //1번
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy());
@@ -33,6 +37,7 @@ public class AppConfig {
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
