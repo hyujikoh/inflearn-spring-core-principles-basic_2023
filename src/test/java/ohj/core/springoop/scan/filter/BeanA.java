6e7354fe0d80ef32cea5cecf8b5d9a57a32d6b44
hyujikoh <dvum0045@gmail.com>
2024-03-11 15:26:11 +0900

    Study - 의존성 스캔 테스트

---
+++ src/test/java/ohj/core/springoop/scan/filter/BeanA.java
@@ -0,0 +1,10 @@
+package ohj.core.springoop.scan.filter;
+
+/**
+ * Author : hyujikoh
+ * CreatedAt : 2024-03-07
+ * Desc :
+ */
+@MyIncludeComponent
+public class BeanA {
+}
---
+++ src/test/java/ohj/core/springoop/scan/filter/MyIncludeComponent.java
@@ -0,0 +1,14 @@
+package ohj.core.springoop.scan.filter;
+
+import java.lang.annotation.*;
+
+/**
+ * Author : hyujikoh
+ * CreatedAt : 2024-03-07
+ * Desc :
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyIncludeComponent {
+}
