71317f5bb590f7b5d8dd0aa50519049f0c4c0761
hyujikoh <dvum0045@gmail.com>
2024-03-12 16:23:05 +0900

    Study - 옵션을 통한 autowired 테스트

---
+++ src/test/java/ohj/core/springoop/autowired/AutowiredTest.java
@@ -0,0 +1,42 @@
+package ohj.core.springoop.autowired;
+
+import ohj.core.springoop.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.lang.Nullable;
+
+import java.util.Optional;
+
+/**
+ * Author : hyujikoh
+ * CreatedAt : 2024-03-12
+ * Desc :
+ */
+public class AutowiredTest {
+    @Test
+    void AutowiredOption(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+
+    }
+
+
+    static class TestBean {
+        //호출 안됨
+        @Autowired(required = false)
+        public void setNoBean1(Member member) {
+            System.out.println("setNoBean1 = " + member);
+        }
+        //null 호출
+        @Autowired
+        public void setNoBean2(@Nullable Member member) {
+            System.out.println("setNoBean2 = " + member);
+        }
+        //Optional.empty 호출
+        @Autowired(required = false)
+        public void setNoBean3(Optional<Member> member) {
+            System.out.println("setNoBean3 = " + member);
+        }
+    }
+}
