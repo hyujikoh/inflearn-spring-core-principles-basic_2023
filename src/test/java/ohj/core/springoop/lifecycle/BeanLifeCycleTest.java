8a18ee8ae5e1b460e40adb101306870d6d29d8ca
hyujikoh <dvum0045@gmail.com>
2024-03-13 20:39:07 +0900

    Study - 빈 생명주기

---
+++ src/test/java/ohj/core/springoop/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,33 @@
+package ohj.core.springoop.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Author : hyujikoh
+ * CreatedAt : 2024-03-13
+ * Desc :
+ */
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest(){
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient networkClient = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig{
+        @Bean
+        public  NetworkClient networkClient(){
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("https://oop-spring-dev");
+            return networkClient;
+        }
+    }
+}
---
+++ src/test/java/ohj/core/springoop/lifecycle/NetworkClient.java
@@ -0,0 +1,43 @@
+package ohj.core.springoop.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+/**
+ * Author : hyujikoh
+ * CreatedAt : 2024-03-13
+ * Desc :
+ */
+public class NetworkClient implements InitializingBean, DisposableBean {
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+    }
+
+    //서비스 시작시 호출
+    public void connect() {
+        System.out.println("connect: " + url);
+    }
+    public void call(String message) {
+        System.out.println("call: " + url + " message = " + message);
+    }
+    //서비스 종료시 호출
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+    }
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
+    }
+}
